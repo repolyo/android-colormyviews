045a40777356838048a9a0d922ed000196c32871
Christopher Tan <chritan@lexmark.com>
2021-01-22 21:36:37 +0800

    exercise: Add Baseline Constraint and Button Chain
    1. Below the set of boxes, add two TextViews; think of a label and some information
    2. Add a Baseline Constraint from the label to the info.
    3. To situate these TextViews in the layout, add a fixed constraint of 16dp between the label and the info.
    4. Constrain the top of info TextView to the bottom of BoxTwo, and use bias to vertically place it in the layout.
    5. Add a horizontal chain of three buttons constrained to the bottom of the layout. Add any necessary constraints.
    6. Test your layout by choosing different devices and orientations.
    7. Add the code shown below to the click handler.

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,11 +4,32 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var red_button: View;
+    lateinit var yellow_button: View;
+    lateinit var green_button: View;
+
+    lateinit var box_one_text: View;
+    lateinit var box_two_text: View;
+    lateinit var box_three_text: View;
+    lateinit var box_four_text: View;
+    lateinit var box_five_text: View;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        box_one_text = findViewById(R.id.box_one_text)
+        box_two_text = findViewById(R.id.box_two_text)
+        box_three_text = findViewById(R.id.box_three_text)
+        box_four_text = findViewById(R.id.box_four_text)
+        box_five_text = findViewById(R.id.box_five_text)
+
+        red_button = findViewById(R.id.red_button)
+        yellow_button = findViewById(R.id.yellow_button)
+        green_button = findViewById(R.id.green_button)
 
         setListeners()
     }
@@ -16,12 +37,14 @@ class MainActivity : AppCompatActivity() {
     private
     fun setListeners() {
         val clickableViews: List<View> =
-                listOf(findViewById(R.id.box_one_text),
-                        findViewById(R.id.box_two_text),
-                        findViewById(R.id.box_three_text),
-                        findViewById(R.id.box_four_text),
-                        findViewById(R.id.box_five_text),
-                        findViewById(R.id.constraint_layout))
+                listOf(box_one_text,
+                        box_two_text,
+                        box_three_text,
+                        box_four_text,
+                        box_five_text,
+                        red_button,
+                        yellow_button,
+                        green_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColorClickListener(item) }
@@ -40,6 +63,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
